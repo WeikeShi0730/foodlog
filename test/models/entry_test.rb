81372f26fe7ece7f83dfae772bda1a7857a2cf34
Weike Shi <weike.shi@ericsson.com>
2021-08-19 14:07:39 -0400

    changed database to postgresql and added tests

--- test/models/entry_test.rb
+++ test/models/entry_test.rb
@@ -1,7 +1,14 @@
 require "test_helper"
 
 class EntryTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "is valid with valid attributes" do
+    entry = Entry.new(meal_type: "breakfast", carbs: 35, proteins: 20, fats: 10, calories: 700)
+    assert entry.save
+  end
+
+  test "should not save entry without calories" do
+    entry = Entry.new(meal_type: "breakfast", carbs: 35, proteins: 20, fats: 10)
+    assert_not entry.save
+  end
+
 end
--- test/system/entries_test.rb
+++ test/system/entries_test.rb
@@ -2,48 +2,46 @@ require "application_system_test_case"
 
 class EntriesTest < ApplicationSystemTestCase
   setup do
-    @entry = entries(:one)
+    @entry = entries(:breakfast)
   end
 
   test "visiting the index" do
     visit entries_url
-    assert_selector "h1", text: "Entries"
+    assert_selector "h1", text: "foodlog – #{DateTime.now.to_s(:nice_date).downcase}"
   end
 
   test "creating a Entry" do
     visit entries_url
-    click_on "New Entry"
+    click_on "create new entry"
 
     fill_in "Calories", with: @entry.calories
     fill_in "Carbs", with: @entry.carbs
     fill_in "Fats", with: @entry.fats
     fill_in "Meal type", with: @entry.meal_type
-    fill_in "proteins", with: @entry.proteins
+    fill_in "Proteins", with: @entry.proteins
     click_on "Create Entry"
 
     assert_text "Entry was successfully created"
-    click_on "Back"
   end
 
   test "updating a Entry" do
     visit entries_url
-    click_on "Edit", match: :first
+    click_on "edit", match: :first
 
     fill_in "Calories", with: @entry.calories
     fill_in "Carbs", with: @entry.carbs
     fill_in "Fats", with: @entry.fats
     fill_in "Meal type", with: @entry.meal_type
-    fill_in "proteins", with: @entry.proteins
+    fill_in "Proteins", with: @entry.proteins
     click_on "Update Entry"
 
     assert_text "Entry was successfully updated"
-    click_on "Back"
   end
 
   test "destroying a Entry" do
     visit entries_url
     page.accept_confirm do
-      click_on "Destroy", match: :first
+      click_on "destroy", match: :first
     end
 
     assert_text "Entry was successfully destroyed"
